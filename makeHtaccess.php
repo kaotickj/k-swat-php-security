9a129ff08f9dbb204f4916fd326c9aac6bfa6984
Johnny Watts <58899228+kaotickj@users.noreply.github.com>
2019-12-15 22:46:29 -0500

    Update makeHtaccess.php

--- makeHtaccess.php
+++ makeHtaccess.php
@@ -1,10 +1,10 @@
 <?php
-unlink ('index.html');
 if (!file_exists('kblocker/vault/counter.dat')) {
     $file = 'kblocker/vault/counter.dat';
     $content = '0';
         file_put_contents($file, $content, FILE_APPEND | LOCK_EX);
 } else {
+        unlink ('index.html');
     $dir = 'kblocker/vault/';
     $file = $dir.'.htaccess';
     $kill = $dir.'htaccess.txt';
