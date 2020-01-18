cbdcde3471c8d5f1c7225d5401a27de31db61590
Johnny Watts <58899228+kaotickj@users.noreply.github.com>
2020-01-18 15:22:54 -0500

    Update not-a-bot.php

--- kblocker/bot-trap/not-a-bot.php
+++ kblocker/bot-trap/not-a-bot.php
@@ -19,10 +19,10 @@
 						}	
 //  <===== END JOHNNY 5 CHECK ====> //
 
-				session_start();				
-				if($_POST['puzzle'] != $_SESSION['digit']) die("<head><title>Thank You for Contacting Us</title><link type='text/css' rel='stylesheet' href='../css/resp-style.css'/></head><body style='color:#fff;'><p><img src='../Images/stories/oops.png' alt='Wrong Spam Prevention Code' style='display:block; margin-left:auto; margin-right:auto; width:280px;' /></p><br><br><p>Sorry, the Spam Prevention Code entered was incorrect! Use the  button below to return to the form and try again. If you are having trouble seeing the digits, try refreshing the Puzzle image using the refresh button beside the Spam Prevention Code.<br><br><button class='buttons' onClick='nav(-1)'>Back to Form</button></p>");
+/*				session_start();				
+				if($_POST['puzzle'] != $_SESSION['digits']) die("<head><title>Thank You for Contacting Us</title><link type='text/css' rel='stylesheet' href='../css/resp-style.css'/></head><body style='color:#fff;'><p><img src='../Images/stories/oops.png' alt='Wrong Spam Prevention Code' style='display:block; margin-left:auto; margin-right:auto; width:280px;' /></p><br><br><p>Sorry, the Spam Prevention Code entered was incorrect! Use the  button below to return to the form and try again. If you are having trouble seeing the digits, try refreshing the Puzzle image using the refresh button beside the Spam Prevention Code.<br><br><button class='buttons' onClick='nav(-1)'>Back to Form</button></p>");
 				session_destroy();
-
+*/
 
 
 // This function is used to filter harmful input and secure the form data before storing the data or sending it to your email. Usage: $str = secure_input($_POST['input']); Where $str is your unique PHP variable assigned to the input (i.e., $firstName) and 'input' is the "name" attribute from your form input field (i.e., $_POST['first_name']).  For the example used: $firstName = secure_input($_POST['first_name'] Do not use spaces, hyphens, underscores, etc., in PHP variables.
@@ -33,9 +33,8 @@
 			  $data = htmlspecialchars($data);
 			  return $data;
 			}		
-//			die('Popcorn');
-	echo '<meta http-equiv="refresh" content="0; url=/" />';
-			   } else {
-echo '<meta http-equiv="refresh" content="0; url=https://kdgwebdesign.com/not-a-bot/wp-im_a_bot" />';				   
+		//	die('Popcorn');
+	echo '<meta http-equiv="refresh" content="0; url=/index.php" />';
 			   }
+
 ?>			   
